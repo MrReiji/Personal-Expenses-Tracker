bc76a765bee8f136201b70e341fd79f4af237000
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2022-08-14 00:14:43 +0200

    Extract Widgets

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -102,10 +102,11 @@ class _NewTransactionState extends State<NewTransaction> {
                   ],
                 ),
               ),
-              RaisedButton(
+              ElevatedButton(
                 child: Text('Add Transaction'),
-                color: Theme.of(context).primaryColor,
-                textColor: Theme.of(context).textTheme.button.color,
+                style: ElevatedButton.styleFrom(
+                    textStyle: TextStyle(color: Theme.of(context).primaryColor),
+                    primary: Theme.of(context).primaryColor),
                 onPressed: _submitData,
               ),
             ],
